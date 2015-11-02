b93d1a05973c7dfb4c91c24384979b14e2a56875
Francois Beaufort <beaufort.francois@gmail.com>
2015-11-02 16:35:26 +0100

    Added UI for heart rate sensor

--- web-bluetooth/heart-rate-sensor/app.js
+++ web-bluetooth/heart-rate-sensor/app.js
@@ -1,43 +1,68 @@
-var maxHeartRate = null;
-var minHeartRate = null;
+var canvas = document.querySelector('canvas');
+var statusText = document.querySelector('#statusText');
 
-document.querySelector('#connect').addEventListener('click', function() {
-  document.querySelector('#device').textContent = '';
-  document.querySelector('#heartRate').textContent = '';
-  document.querySelector('#bodySensorLocation').textContent = '';
+statusText.addEventListener('click', function() {
+  statusText.textContent = 'Breath...';
+  heartRates = [];
   heartRateSensor.connect()
-  .then(() => {
-    console.log(heartRateSensor.device);
-    document.querySelector('#device').textContent = heartRateSensor.device.instanceID;
-    return Promise.all([
-      heartRateSensor.getBodySensorLocation().then(handleBodySensorLocation),
-      heartRateSensor.startNotificationsHeartRateMeasurement().then(handleNotificationsHeartRateMeasurement),
-    ]);
-  })
+  .then(() => heartRateSensor.startNotificationsHeartRateMeasurement().then(handleHeartRateMeasurement))
   .catch(error => {
-    console.error('Argh!', error);
+    statusText.textContent = error;
   });
 });
 
-function handleNotificationsHeartRateMeasurement(heartRateMeasurement) {
-  heartRateMeasurement.addEventListener('characteristicvaluechanged', function(heartRate) {
-    var heartRate = heartRateSensor.parseHeartRate(event.target.value);
-    document.querySelector('#heartRate').textContent = JSON.stringify(heartRate, null, 2);
-    if (maxHeartRate === null) {
-      maxHeartRate = heartRate.heartRate;
-    } else {
-      maxHeartRate = Math.max(maxHeartRate, heartRate.heartRate);
-    }
-    document.querySelector('#maxHeartRate').textContent = 'Max ' + maxHeartRate + ' BPM';
-    if (minHeartRate === null) {
-      minHeartRate = heartRate.heartRate;
-    } else {
-      minHeartRate = Math.min(minHeartRate, heartRate.heartRate);
-    }
-    document.querySelector('#minHeartRate').textContent = 'Min ' + minHeartRate + ' BPM';
+function handleHeartRateMeasurement(heartRateMeasurement) {
+  heartRateMeasurement.addEventListener('characteristicvaluechanged', event => {
+    var heartRateMeasurement = heartRateSensor.parseHeartRate(event.target.value);
+    statusText.innerHTML = heartRateMeasurement.heartRate + ' &#x2764;';
+    heartRates.push(heartRateMeasurement.heartRate);
+    drawWaves();
   });
 }
 
-function handleBodySensorLocation(bodySensorLocation) {
-  document.querySelector('#bodySensorLocation').textContent = 'Body sensor is placed on the ' + bodySensorLocation + '.';
+var heartRates = [];
+var mode = 'bar';
+
+canvas.addEventListener('click', event => {
+  mode = mode === 'bar' ? 'line' : 'bar';
+  drawWaves();
+});
+
+function drawWaves() {
+  requestAnimationFrame(() => {
+    canvas.width = parseInt(getComputedStyle(canvas).width.slice(0, -2)) * devicePixelRatio;
+    canvas.height = parseInt(getComputedStyle(canvas).height.slice(0, -2)) * devicePixelRatio;
+
+    var context = canvas.getContext('2d');
+    var margin = 2;
+    var max = Math.max(0, Math.round(canvas.width / 11));
+    var offset = Math.max(0, heartRates.length - max);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeStyle = '#00796B';
+    if (mode === 'bar') {
+      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
+        var barHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
+        context.rect(11 * i + margin, canvas.height - barHeight, margin, Math.max(0, barHeight - margin));
+        context.stroke();
+      }
+    } else if (mode === 'line') {
+      context.beginPath();
+      context.lineWidth = 6;
+      context.lineJoin = 'round';
+      context.shadowBlur = '1';
+      context.shadowColor = '#333';
+      context.shadowOffsetY = '1';
+      for (var i = 0; i < Math.max(heartRates.length, max); i++) {
+        var lineHeight = Math.round(heartRates[i + offset ] * canvas.height / 200);
+        if (i === 0) {
+          context.moveTo(11 * i, canvas.height - lineHeight);
+        } else {
+          context.lineTo(11 * i, canvas.height - lineHeight);
+        }
+        context.stroke();
+      }
+    }
+  });
 }
+
+window.onresize = drawWaves;
