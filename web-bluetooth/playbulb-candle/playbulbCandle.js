d1cdb19031233434dc5e54a520425167e8df8efa
Francois Beaufort <beaufort.francois@gmail.com>
2015-11-23 13:23:05 +0100

    Removed cached characteristics strategy

--- web-bluetooth/playbulb-candle/playbulbCandle.js
+++ web-bluetooth/playbulb-candle/playbulbCandle.js
@@ -17,9 +17,7 @@
   class PlaybulbCandle {
     constructor() {
       this.device = null;
-      this.server = null;
-      this._characteristics = new Map();
-      this._debug = false;
+      this.gattServer = null;
     }
     connect() {
       return navigator.bluetooth.requestDevice({filters:[{services:[ CANDLE_SERVICE_UUID ]}]})
@@ -27,60 +25,45 @@
         this.device = device;
         return device.connectGATT();
       })
-      .then(server => {
-        this.server = server;
-        return Promise.all([
-          server.getPrimaryService(CANDLE_SERVICE_UUID).then(service => {
-            return Promise.all([
-              this._cacheCharacteristic(service, CANDLE_DEVICE_NAME_UUID),
-              this._cacheCharacteristic(service, CANDLE_COLOR_UUID),
-              this._cacheCharacteristic(service, CANDLE_EFFECT_UUID),
-            ])
-          }),
-          server.getPrimaryService('battery_service').then(service => {
-            return this._cacheCharacteristic(service, 'battery_level')
-          }),
-          // TODO: Uncomment when device_information service is actually
-          // available in Chrome OS. http://crbug.com/532930
-          /*
-          server.getPrimaryService('device_information').then(service => {
-            return Promise.all([
-              this._cacheCharacteristic(service, 'serial_number_string'),
-              this._cacheCharacteristic(service, 'hardware_revision_string'),
-              this._cacheCharacteristic(service, 'firmware_revision_string'),
-              this._cacheCharacteristic(service, 'software_revision_string'),
-              this._cacheCharacteristic(service, 'manufacturer_name_string'),
-              this._cacheCharacteristic(service, 'pnp_id'),
-            ])
-          }),
-          */
-        ]);
+      .then(gattServer => {
+        //TODO: Remove when gattServer is added to device.
+        this.gattServer = gattServer;
       })
     }
 
     /* Candle Service */
 
     getDeviceName() {
-      return this._readCharacteristicValue(CANDLE_DEVICE_NAME_UUID)
-      .then(this._decodeString);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_DEVICE_NAME_UUID))
+      .then(characteristic => characteristic.readValue())
+      .then(this._decodeString)
     }
     setDeviceName(name) {
       let data = this._encodeString(name);
-      return this._writeCharacteristicValue(CANDLE_DEVICE_NAME_UUID, data)
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_DEVICE_NAME_UUID))
+      .then(characteristic => characteristic.writeValue(data))
     }
     setColor(r, g, b) {
-      let data = [0x00, r, g, b];
-      return this._writeCharacteristicValue(CANDLE_COLOR_UUID, new Uint8Array(data))
+      let data = new Uint8Array([0x00, r, g, b]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_COLOR_UUID))
+      .then(characteristic => characteristic.writeValue(data))
       .then(() => [r,g,b]); // Returns color when fulfilled.
     }
     setCandleEffectColor(r, g, b) {
-      let data = [0x00, r, g, b, 0x04, 0x00, 0x01, 0x00];
-      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      let data = new Uint8Array([0x00, r, g, b, 0x04, 0x00, 0x01, 0x00]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_EFFECT_UUID))
+      .then(characteristic => characteristic.writeValue(data))
       .then(() => [r,g,b]); // Returns color when fulfilled.
     }
     setFlashingColor(r, g, b) {
-      let data = [0x00, r, g, b, 0x00, 0x00, 0x1F, 0x00];
-      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      let data = new Uint8Array([0x00, r, g, b, 0x00, 0x00, 0x1F, 0x00]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_EFFECT_UUID))
+      .then(characteristic => characteristic.writeValue(data))
       .then(() => [r,g,b]); // Returns color when fulfilled.
     }
     setPulseColor(r, g, b) {
@@ -88,81 +71,79 @@
       let newRed = Math.min(Math.round(r / 64) * 64, 255);
       let newGreen = Math.min(Math.round(g / 64) * 64, 255);
       let newBlue = Math.min(Math.round(b / 64) * 64, 255);
-      let data = [0x00, newRed, newGreen, newBlue, 0x01, 0x00, 0x09, 0x00];
-      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      let data = new Uint8Array([0x00, newRed, newGreen, newBlue, 0x01, 0x00, 0x09, 0x00]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_EFFECT_UUID))
+      .then(characteristic => characteristic.writeValue(data))
       .then(() => [r,g,b]); // Returns color when fulfilled.
     }
     setRainbow() {
-      let data = [0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0x00];
-      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data));
+      let data = new Uint8Array([0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0x00]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_EFFECT_UUID))
+      .then(characteristic => characteristic.writeValue(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
     }
     setRainbowFade() {
-      let data = [0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x26, 0x00];
-      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data));
+      let data = new Uint8Array([0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x26, 0x00]);
+      return this.gattServer.getPrimaryService(CANDLE_SERVICE_UUID)
+      .then(service => service.getCharacteristic(CANDLE_EFFECT_UUID))
+      .then(characteristic => characteristic.writeValue(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
     }
 
     /* Battery Service */
 
     getBatteryLevel() {
-      return this._readCharacteristicValue('battery_level')
-      .then(data => data.getUint8(0));
+      return this.gattServer.getPrimaryService('battery_service')
+      .then(service => service.getCharacteristic('battery_level'))
+      .then(characteristic => characteristic.readValue())
+      .then(buffer => new DataView(buffer).getUint8(0));
     }
 
     /* Device Info Service */
 
     getSerialNumber() {
-      return this._readCharacteristicValue('serial_number_string')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('serial_number_string'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
     getHardwareRevision() {
-      return this._readCharacteristicValue('hardware_revision_string')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('hardware_revision_string'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
     getFirmwareRevision() {
-      return this._readCharacteristicValue('firmware_revision_string')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('firmware_revision_string'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
     getSoftwareRevision() {
-      return this._readCharacteristicValue('software_revision_string')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('software_revision_string'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
     getManufacturerName() {
-      return this._readCharacteristicValue('manufacturer_name_string')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('manufacturer_name_string'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
     getPnpID() {
-      return this._readCharacteristicValue('pnp_id')
+      return this.gattServer.getPrimaryService('device_information')
+      .then(service => service.getCharacteristic('pnp_id'))
+      .then(characteristic => characteristic.readValue())
       .then(this._decodeString);
     }
 
     /* Utils */
 
-    _cacheCharacteristic(service, characteristicUuid) {
-      return service.getCharacteristic(characteristicUuid)
-      .then(characteristic => {
-        this._characteristics.set(characteristicUuid, characteristic);
-      });
-    }
-    _readCharacteristicValue(characteristicUuid) {
-      let characteristic = this._characteristics.get(characteristicUuid);
-      return characteristic.readValue()
-      .then(buffer => {
-        let data = new DataView(buffer);
-        if (this._debug) {
-          for (var i = 0, a = []; i < data.byteLength; i++) { a.push(data.getUint8(i)); }
-          console.debug('READ', characteristic.uuid, a);
-        }
-        return data;
-      });
-    }
-    _writeCharacteristicValue(characteristicUuid, value) {
-      let characteristic = this._characteristics.get(characteristicUuid);
-      if (this._debug) {
-        console.debug('WRITE', characteristic.uuid, value);
-      }
-      return characteristic.writeValue(value);
-    }
-    _decodeString(data) {
+    _decodeString(buffer) {
+      let data = new DataView(buffer);
       return decoder.decode(data);
     }
     _encodeString(data) {
