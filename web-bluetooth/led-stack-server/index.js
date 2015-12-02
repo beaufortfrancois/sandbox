47a7f87fc937a4cb2cc26baa1e5a5a0b1cdad387
Francois Beaufort <beaufort.francois@gmail.com>
2015-12-02 10:31:36 +0100

    Fixed Beacon support

--- web-bluetooth/led-stack-server/index.js
+++ web-bluetooth/led-stack-server/index.js
@@ -2,7 +2,9 @@ var Nascent = require('nascent');
 var ledbar = Nascent.getModule('LED Bar');
 
 var util = require('util');
-var bleno = require('bleno');
+var eddystone = require('eddystone-beacon');
+/* https://github.com/don/node-eddystone-beacon/pull/31#issue-119847706 */
+var bleno = require('eddystone-beacon/node_modules/bleno');
 
 function dropLed() {
   var numLeds = ledbar.getNumLeds();
@@ -122,59 +124,58 @@ function MessagesCharacteristic() {
 
 util.inherits(MessagesCharacteristic, bleno.Characteristic);
 
-function MainService() {
-   var self = this;
-   bleno.PrimaryService.call(this, {
-       uuid: 'ec00',
-       characteristics: [
-           new ColorCharacteristic(),
-           new MessagesCharacteristic()
-       ]
-   });
-}
-
-util.inherits(MainService, bleno.PrimaryService);
-
-var mainService = new MainService();
-
-bleno.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    bleno.startAdvertising('LED Stack', [mainService.uuid], function(err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-  else {
-    bleno.stopAdvertising();
-  }
-});
-
-bleno.on('advertisingStart', function(err) {
-  console.log('advertisingStart');
-  updateLeds();
-  if (!err) {
-    bleno.setServices([mainService]);
-  }
-});
-
 var clientAddress = null;
 var users = {};
 var messages = [];
 
 const colors = [{"r":242,"g":214,"b":214},{"r":80,"g":5,"b":5},{"r":80,"g":15,"b":15},{"r":81,"g":255,"b":255},{"r":213,"g":175,"b":175},{"r":215,"g":63,"b":63},{"r":11,"g":15,"b":15},{"r":14,"g":95,"b":95},{"r":222,"g":155,"b":155},{"r":78,"g":113,"b":113},{"r":111,"g":240,"b":240},{"r":111,"g":240,"b":240},{"r":254,"g":160,"b":160},{"r":252,"g":64,"b":64},{"r":249,"g":16,"b":16},{"r":243,"g":208,"b":208}];
 
+bleno.on('stateChange', function(state) {
+  console.log('stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertising('LED Stack', ['ec00']);
+    startBeacon();
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function(error) {
+  console.log('advertisingStart: ' + (error ? 'error ' + error : 'success'));
+  updateLeds();
+
+  if (!error) {
+    bleno.setServices([
+      new bleno.PrimaryService({
+        uuid: 'ec00',
+        characteristics: [
+          new ColorCharacteristic(),
+          new MessagesCharacteristic()
+        ]
+      })
+    ]);
+  }
+});
+
+function startBeacon() {
+  console.log("Starting beacon.");
+  eddystone.advertiseUrl('https://goo.gl/fkD0WM');
+}
+
 bleno.on('accept', function(address) {
-  console.log('accept ' + address);
+  console.log("Accepting connection. Stopping beacon.");
+  eddystone.stop();
+
   clientAddress = address;
   if (!users[clientAddress]) {
     var color = colors[Math.round(Math.random(colors.length) * colors.length)];
     users[clientAddress] = {'r': color.r, 'g': color.g, 'b': color.b};
   }
-  console.log(users);
 });
 
 bleno.on('disconnect', function(address) {
   clientAddress = null;
-  console.log('disconnect ' + address);
-});
+  console.log("Client disconnected ...");
+  startBeacon();
+})
