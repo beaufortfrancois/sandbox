04c762cbafc12180a7a4cad4114ef37fa23a2b1d
Francois Beaufort <beaufort.francois@gmail.com>
2016-01-07 15:31:16 +0100

    Added gatt characteristics read helpers

---
+++ web-bluetooth/utils/utils.js
@@ -0,0 +1,72 @@
+BluetoothGATTCharacteristic.prototype.readFloat32Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getFloat32(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readFloat64Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getFloat64(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readInt16Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getInt16(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readInt32Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getInt32(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readInt8Value = function(byteOffset) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getInt8(byteOffset);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readStringValue = function(utfLabel = 'utf8') {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    var decoder = new TextDecoder(utfLabel);
+    return decoder.decode(data);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readUint16Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getUint16(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readUint32Value = function(byteOffset, littleEndian = false) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getUint32(byteOffset, littleEndian);
+  });
+};
+
+BluetoothGATTCharacteristic.prototype.readUint8Value = function(byteOffset) {
+  return this.readValue()
+  .then(buffer => {
+    var data = new DataView(buffer);
+    return data.getUint8(byteOffset);
+  });
+};
