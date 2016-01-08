278d564431e31bdc3bd690e7b0dd07949420f446
Francois Beaufort <beaufort.francois@gmail.com>
2016-01-08 10:39:04 +0100

    s/read/get/... and used cached value

--- web-bluetooth/utils/utils.js
+++ web-bluetooth/utils/utils.js
@@ -1,72 +1,45 @@
-BluetoothGATTCharacteristic.prototype.readFloat32Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getFloat32(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getFloat32Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getFloat32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readFloat64Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getFloat64(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getFloat64Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getFloat64(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readInt16Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getInt16(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getInt16Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getInt16(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readInt32Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getInt32(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getInt32Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getInt32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readInt8Value = function(byteOffset) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getInt8(byteOffset);
-  });
+BluetoothGATTCharacteristic.prototype.getInt8Value = function(byteOffset) {
+  var data = new DataView(this.value);
+  return data.getInt8(byteOffset);
 };
 
-BluetoothGATTCharacteristic.prototype.readStringValue = function(utfLabel = 'utf8') {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    var decoder = new TextDecoder(utfLabel);
-    return decoder.decode(data);
-  });
+BluetoothGATTCharacteristic.prototype.getStringValue = function(utfLabel = 'utf8') {
+  var data = new DataView(this.value);
+  var decoder = new TextDecoder(utfLabel);
+  return decoder.decode(data);
 };
 
-BluetoothGATTCharacteristic.prototype.readUint16Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getUint16(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getUint16Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getUint16(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readUint32Value = function(byteOffset, littleEndian = true) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getUint32(byteOffset, littleEndian);
-  });
+BluetoothGATTCharacteristic.prototype.getUint32Value = function(byteOffset, littleEndian = true) {
+  var data = new DataView(this.value);
+  return data.getUint32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.readUint8Value = function(byteOffset) {
-  return this.readValue()
-  .then(buffer => {
-    var data = new DataView(buffer);
-    return data.getUint8(byteOffset);
-  });
+BluetoothGATTCharacteristic.prototype.getUint8Value = function(byteOffset) {
+  var data = new DataView(this.value);
+  return data.getUint8(byteOffset);
 };
