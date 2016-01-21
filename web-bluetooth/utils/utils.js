af3c96d6cb35f59eefc6ce8b3f081d76817082da
Francois Beaufort <beaufort.francois@gmail.com>
2016-01-21 15:47:45 +0100

    Removed DataView in utils

--- web-bluetooth/utils/utils.js
+++ web-bluetooth/utils/utils.js
@@ -1,45 +1,36 @@
 BluetoothGATTCharacteristic.prototype.getFloat32Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getFloat32(byteOffset, littleEndian);
+  return this.value.getFloat32(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getFloat64Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getFloat64(byteOffset, littleEndian);
+  return this.value.getFloat64(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getInt16Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getInt16(byteOffset, littleEndian);
+  return this.value.getInt16(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getInt32Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getInt32(byteOffset, littleEndian);
+  return this.value.getInt32(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getInt8Value = function(byteOffset) {
-  var data = new DataView(this.value);
-  return data.getInt8(byteOffset);
+  return this.value.getInt8(byteOffset);
 };
 
 BluetoothGATTCharacteristic.prototype.getStringValue = function(utfLabel = 'utf8') {
-  var data = new DataView(this.value);
   var decoder = new TextDecoder(utfLabel);
-  return decoder.decode(data);
+  return decoder.decode(this.value);
 };
 
 BluetoothGATTCharacteristic.prototype.getUint16Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getUint16(byteOffset, littleEndian);
+  return this.value.getUint16(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getUint32Value = function(byteOffset, littleEndian = true) {
-  var data = new DataView(this.value);
-  return data.getUint32(byteOffset, littleEndian);
+  return this.value.getUint32(byteOffset, littleEndian);
 };
 
 BluetoothGATTCharacteristic.prototype.getUint8Value = function(byteOffset) {
-  var data = new DataView(this.value);
-  return data.getUint8(byteOffset);
+  return this.value.getUint8(byteOffset);
 };
