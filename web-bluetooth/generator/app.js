67226db22734bb1f4467d0df3c039ab3f3b2b2e8
Francois Beaufort <beaufort.francois@gmail.com>
2016-04-06 16:58:29 +0200

    Add characteristic #1 to generator

--- web-bluetooth/generator/app.js
+++ web-bluetooth/generator/app.js
@@ -7,21 +7,20 @@ function generateCode(options) {
   var advertisedDeviceName = options.advertisedDeviceName;
   var advertisedDeviceNamePrefix = options.advertisedDeviceNamePrefix;
 
-  var filterOptions = {};
+  var filterOptions = {filters: [{}]};
   if (advertisedDeviceName) {
-    filterOptions.name = advertisedDeviceName;
+    filterOptions.filters[0].name = advertisedDeviceName;
   }
   if (advertisedDeviceNamePrefix) {
-    filterOptions.namePrefix = advertisedDeviceNamePrefix;
+    filterOptions.filters[0].namePrefix = advertisedDeviceNamePrefix;
   }
   if (advertisedServices) {
-    filterOptions.services = Array.of(advertisedServices);
+    filterOptions.filters[0].services = Array.of(advertisedServices);
   }
 
   var characteristicMethods = '';
-  var optionalServices = '';
   if (options.services) {
-    optionalServices = 'options.optionalServices = [' + options.services.map(service => service.uuid) + '];\n';
+    filterOptions.optionalServices = options.services.map(service => service.uuid);
 
     options.services.forEach(service => {
       service.characteristics.forEach(characteristic => {
@@ -29,8 +28,8 @@ function generateCode(options) {
         if (characteristic.properties.read) {
           characteristicMethods += `
   get${characteristicName}() {
-    return this.device.gatt.getPrimaryService(${service.uuid})
-    .then(service => service.getCharacteristic(${characteristic.uuid}))
+    return this.device.gatt.getPrimaryService("${service.uuid}")
+    .then(service => service.getCharacteristic("${characteristic.uuid}"))
     .then(characteristic => characteristic.readValue());
   }
 `;
@@ -38,8 +37,8 @@ function generateCode(options) {
         if (characteristic.properties.write) {
           characteristicMethods += `
   set${characteristicName}(data) {
-    return this.device.gatt.getPrimaryService(${service.uuid})
-    .then(service => service.getCharacteristic(${characteristic.uuid}))
+    return this.device.gatt.getPrimaryService("${service.uuid}")
+    .then(service => service.getCharacteristic("${characteristic.uuid}"))
     .then(characteristic => characteristic.writeValue(data));
   }
 `;
@@ -57,12 +56,11 @@ class ${classDeviceName} {
   }
 
   request() {
-    let options = {filters: [${JSON.stringify(filterOptions)}]};
-    ${optionalServices}
+    let options = ${JSON.stringify(filterOptions)};
     return navigator.bluetooth.requestDevice(options)
     .then(device => {
       this.device = device;
-      return this.device;
+      return device;
     });
   }
 
@@ -92,11 +90,28 @@ var ${instanceDeviceName} = new ${classDeviceName}();
 }
 
 function updateCodePreview() {
+  
   var instanceDeviceName = $('#deviceName').value;
   var classDeviceName = instanceDeviceName.charAt(0).toUpperCase() + instanceDeviceName.slice(1);
-  var advertisedServices = $('#advertisedServices').value;
+  var advertisedServices = $('#advertisedServices').value.split(',').map(e => e.trim()).filter(e => e.length);
   var advertisedDeviceName = $('#advertisedDeviceName').value;
   var advertisedDeviceNamePrefix = $('#advertisedDeviceNamePrefix').value;
+
+  try {
+    advertisedServices.map(str => { 
+      if (str.startsWith('0x')) {
+        BluetoothUUID.getService(parseInt(str.slice(2), 16));
+      } else if (str) {
+        BluetoothUUID.getService(str);
+      }
+    });
+    $('#advertisedServices').setCustomValidity('');
+  } catch(e) {
+    $('#advertisedServices').setCustomValidity(e);
+    $('code').innerText = '';
+    return;
+  }
+
   $('code').innerText = generateCode({
      classDeviceName: classDeviceName, 
      instanceDeviceName: instanceDeviceName,
@@ -105,26 +120,15 @@ function updateCodePreview() {
      advertisedDeviceNamePrefix: advertisedDeviceNamePrefix,
      services: [
        {
-         uuid: '"battery_service"',
+         uuid: $('#characteristic1Service').value,
          characteristics: [
            {
-             uuid: '"battery_level"',
-             name: 'batteryLevel',
+             uuid: $('#characteristic1').value,
+             name: $('#characteristic1Name').value,
              properties: {
-               read: true
-             }
-           }
-         ]
-       },
-       {
-         uuid: '"device_information"',
-         characteristics: [
-           {
-             uuid: '"serial_number_string"',
-             name: 'serialNumber',
-             properties: {
-               read: true,
-               write: true
+               read: $('#characteristic1Read').checked,
+               write: $('#characteristic1Write').checked, 
+               notify: $('#characteristic1Notify').checked, 
              }
            }
          ]
@@ -134,16 +138,34 @@ function updateCodePreview() {
   hljs.highlightBlock($('code'));
 }
 
-document.querySelector('#deviceName').addEventListener('input', updateCodePreview);
-document.querySelector('#advertisedDeviceName').addEventListener('input', updateCodePreview);
-document.querySelector('#advertisedDeviceNamePrefix').addEventListener('input', updateCodePreview);
-document.querySelector('#advertisedServices').addEventListener('input', updateCodePreview);
+$('#deviceName').addEventListener('input', updateCodePreview);
+$('#advertisedDeviceName').addEventListener('input', updateCodePreview);
+$('#advertisedDeviceNamePrefix').addEventListener('input', updateCodePreview);
+$('#advertisedServices').addEventListener('input', updateCodePreview);
+$('#characteristic1Name').addEventListener('input', updateCodePreview);
+$('#characteristic1Service').addEventListener('input', updateCodePreview);
+$('#characteristic1').addEventListener('input', updateCodePreview);
+$('#characteristic1Read').addEventListener('change', updateCodePreview);
+$('#characteristic1Write').addEventListener('change', updateCodePreview);
+$('#characteristic1Notify').addEventListener('change', updateCodePreview);
 
 
 function setValue(inputId, value) {
   $('#' + inputId).value = value;
 }
 
-setValue('deviceName', 'bluetoothDevice');
+function setCheck(inputId, value) {
+  $('#' + inputId).checked = value;
+}
+
+setValue('deviceName', 'myBluetoothDevice');
+setValue('advertisedDeviceName', ' ');
 setValue('advertisedServices', 'battery_service');
+setValue('characteristic1Name', 'serialNumber');
+setValue('characteristic1Service', 'device_information');
+setValue('characteristic1', 'serial_number_string');
+setCheck('characteristic1Read', true);
+setCheck('characteristic1Write', true);
+setCheck('characteristic1Notify', false);
+
 updateCodePreview();
