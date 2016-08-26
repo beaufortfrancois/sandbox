67403f1d1ca257ff25ff846129ac24b3bb353b9c
Francois Beaufort <beaufort.francois@gmail.com>
2016-08-26 14:45:05 +0200

    Add BluetoothRemoteGATTService.prototype.getCharacteristics

--- web-bluetooth/utils/utils.js
+++ web-bluetooth/utils/utils.js
@@ -1,36 +1,56 @@
-BluetoothGATTCharacteristic.prototype.getFloat32Value = function(byteOffset, littleEndian = true) {
+/* BluetoothRemoteGATTCharacteristic helpers */
+
+BluetoothRemoteGATTCharacteristic.prototype.getFloat32Value = function(byteOffset, littleEndian = true) {
   return this.value.getFloat32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getFloat64Value = function(byteOffset, littleEndian = true) {
+BluetoothRemoteGATTCharacteristic.prototype.getFloat64Value = function(byteOffset, littleEndian = true) {
   return this.value.getFloat64(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getInt16Value = function(byteOffset, littleEndian = true) {
+BluetoothRemoteGATTCharacteristic.prototype.getInt16Value = function(byteOffset, littleEndian = true) {
   return this.value.getInt16(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getInt32Value = function(byteOffset, littleEndian = true) {
+BluetoothRemoteGATTCharacteristic.prototype.getInt32Value = function(byteOffset, littleEndian = true) {
   return this.value.getInt32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getInt8Value = function(byteOffset) {
+BluetoothRemoteGATTCharacteristic.prototype.getInt8Value = function(byteOffset) {
   return this.value.getInt8(byteOffset);
 };
 
-BluetoothGATTCharacteristic.prototype.getStringValue = function(utfLabel = 'utf8') {
+BluetoothRemoteGATTCharacteristic.prototype.getStringValue = function(utfLabel = 'utf8') {
   var decoder = new TextDecoder(utfLabel);
   return decoder.decode(this.value);
 };
 
-BluetoothGATTCharacteristic.prototype.getUint16Value = function(byteOffset, littleEndian = true) {
+BluetoothRemoteGATTCharacteristic.prototype.getUint16Value = function(byteOffset, littleEndian = true) {
   return this.value.getUint16(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getUint32Value = function(byteOffset, littleEndian = true) {
+BluetoothRemoteGATTCharacteristic.prototype.getUint32Value = function(byteOffset, littleEndian = true) {
   return this.value.getUint32(byteOffset, littleEndian);
 };
 
-BluetoothGATTCharacteristic.prototype.getUint8Value = function(byteOffset) {
+BluetoothRemoteGATTCharacteristic.prototype.getUint8Value = function(byteOffset) {
   return this.value.getUint8(byteOffset);
 };
+
+/* BluetoothRemoteGATTService helpers */
+
+(function() {
+
+let nativeGetCharacteristics = BluetoothRemoteGATTService.prototype.getCharacteristics;
+let getCharacteristics = function(characteristics) {
+  if (characteristics instanceof Array) {
+    let promises = characteristics.map(characteristic => this.getCharacteristic(characteristic));
+    return Promise.all(promises);
+  }
+  return nativeGetCharacteristics.apply(this, [characteristics]);
+}
+
+window.BluetoothRemoteGATTService.prototype.getCharacteristics = getCharacteristics;
+
+})();
+
