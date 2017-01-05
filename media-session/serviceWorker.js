04fa364325a0bea6fd1cfbb1a6be28227e9d43a4
Francois Beaufort <beaufort.francois@gmail.com>
2017-01-05 16:51:15 +0100

    Add sample with sw

---
+++ media-session/serviceWorker.js
@@ -0,0 +1,53 @@
+// Version 1
+
+var urlsToCache = [
+  'fallbackArtwork.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(initCache());
+});
+
+function initCache() {
+  caches.open('artwork-cache')
+  .then(cache => cache.addAll(urlsToCache))
+}
+
+self.addEventListener('fetch', event => {
+  // TODO: Find a better filter for artwork.
+  if (event.request.url.endsWith('jpg')) {
+    event.respondWith(handleFetchArtwork(event.request));
+  }
+});
+
+function handleFetchArtwork(request) {
+  return getCacheArtwork(request)
+  .then(cacheResponse => cacheResponse || addArtworkToCacheAndGetFallbackArtwork(request));
+}
+
+function getCacheArtwork(request) {
+  return caches.open('artwork-cache')
+  .then(cache => cache.match(request));
+}
+
+function addArtworkToCacheAndGetFallbackArtwork(request) {
+  // Start fetching artwork in parallel...
+  fetch(request)
+  .then(response => {
+    if (response.status === 200) {
+      // Add artwork to the cache for later use.
+      addArtworkToCache(request, response);
+      // Ask clients to update media session artwork.
+      self.clients.matchAll()
+      .then(clients => clients.forEach(client => client.postMessage({'artwork': request.url})));
+    }
+  });
+  // Return fallback artwork.
+  return getCacheArtwork('fallbackArtwork.png');
+}
+
+function addArtworkToCache(request, response) {
+  return caches.open('artwork-cache')
+  .then(cache => cache.put(request, response));
+}
