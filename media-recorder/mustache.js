348a29bf05bb83fa8484fd2e029be37bee6299bb
Francois Beaufort <beaufort.francois@gmail.com>
2017-05-03 09:56:59 +0200

    Fix nit

--- media-recorder/mustache.js
+++ media-recorder/mustache.js
@@ -1,5 +1,9 @@
 self.addEventListener('message', async event => {
   const faceDetector = new FaceDetector({ fastMode: true, maxDetectedFaces: 1 });
   const faces = await faceDetector.detect(event.data);
-  self.postMessage({faces: faces.map(face => face.boundingBox)});
+  // HACK: I wish faces could be cloned.
+  const message = {
+    faces: JSON.stringify(faces.map(face => face.boundingBox))
+  }
+  self.postMessage(message);
 });
