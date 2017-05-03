a6a2b1038f0ba0302af401f21e965fd9747cc216
Francois Beaufort <beaufort.francois@gmail.com>
2017-05-03 13:12:41 +0200

    Handle error when face detection is not supported

--- media-recorder/mustache.js
+++ media-recorder/mustache.js
@@ -1,9 +1,13 @@
 self.addEventListener('message', async event => {
-  const faceDetector = new FaceDetector({ fastMode: true, maxDetectedFaces: 1 });
-  const faces = await faceDetector.detect(event.data);
-  // HACK: I wish faces could be cloned.
-  const message = {
-    faces: JSON.stringify(faces.map(face => face.boundingBox))
+  try {
+    const faceDetector = new FaceDetector({ fastMode: true, maxDetectedFaces: 1 });
+    const faces = await faceDetector.detect(event.data);
+    // HACK: I wish faces could be cloned.
+    const message = {
+      faces: JSON.stringify(faces.map(face => face.boundingBox))
+    }
+    self.postMessage(message);
+  } catch(error) {
+    self.postMessage({ error: error.toString() });
   }
-  self.postMessage(message);
 });
