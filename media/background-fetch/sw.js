f0df9a6e572304e18f8efabefdbde87eba7dcbfd
Francois Beaufort <beaufort.francois@gmail.com>
2017-06-16 14:13:49 +0200

    Use blob instead of arrayBuffer

--- media/background-fetch/sw.js
+++ media/background-fetch/sw.js
@@ -1,13 +1,15 @@
-addEventListener('install', event => {
-  const channel = new BroadcastChannel('channel');
-  channel.postMessage({ status: 'Installing service worker...' });
+useArrayBuffer = false;
 
+addEventListener('install', event => {
   skipWaiting();
   event.waitUntil(async function() {
     // Cache some static files first for offline.
     const cache = await caches.open('files-cache');
     await cache.addAll(['./', 'index.html']);
   }());
+
+  const channel = new BroadcastChannel('channel');
+  channel.postMessage({ status: 'Installed service worker.' });
 });
 
 addEventListener('activate', event => {
@@ -21,6 +23,7 @@ addEventListener('fetch', event => {
 
 async function loadFromCacheOrFetch(request) {
   const channel = new BroadcastChannel('channel');
+  channel.postMessage({ status: 'Fetch request for ' + request.url });
 
   const response = await caches.match(request);
 
@@ -33,23 +36,34 @@ async function loadFromCacheOrFetch(request) {
   // Browser asks for a range request. Let's provide one reconstructed manually from the cache.
   if (request.headers.has('range')) {
     channel.postMessage({ status: 'Getting response from cache in memory...' });
-    // ARGH! See https://github.com/whatwg/fetch/issues/554
-    const data = await response.arrayBuffer();
-    channel.postMessage({ status: '> Got ' + Math.round(data.byteLength / 1024 / 1024) + 'MB' });
-    const pos = Number(/^bytes\=(\d+)\-$/g.exec(request.headers.get('range'))[1]);
-    const options = {
-      status: 206,
-      statusText: 'Partial Content',
-      headers: { 'Content-Range': 'bytes ' + pos + '-' + (data.byteLength - 1) + '/' + data.byteLength }
+    if (useArrayBuffer) {
+      // ARGH! See https://github.com/whatwg/fetch/issues/554
+      const data = await response.arrayBuffer();
+      channel.postMessage({ status: 'Got ' + Math.round(data.byteLength / 1024 / 1024) + 'MB using arrayBuffer()' });
+      const pos = Number(/^bytes\=(\d+)\-$/g.exec(request.headers.get('range'))[1]);
+      const options = {
+        status: 206,
+        statusText: 'Partial Content',
+        headers: { 'Content-Range': 'bytes ' + pos + '-' + (data.byteLength - 1) + '/' + data.byteLength }
+      }
+      return new Response(data.slice(pos), options);
+    } else {
+      const data = await response.blob();
+      channel.postMessage({ status: 'Got ' + Math.round(data.size / 1024 / 1024) + 'MB using blob()' });
+      const pos = Number(/^bytes\=(\d+)\-$/g.exec(request.headers.get('range'))[1]);
+      const options = {
+        status: 206,
+        statusText: 'Partial Content',
+        headers: { 'Content-Range': 'bytes ' + pos + '-' + (data.size - 1) + '/' + data.size }
+      }
+      return new Response(data.slice(pos), options);
     }
-    return new Response(data.slice(pos), options);
   }
 
   // Return cached response.
   channel.postMessage({ status: 'Got response from cache.' });
   return response;
 }
-
 
 addEventListener('backgroundfetched', event => {
   event.waitUntil(async function() {
@@ -60,5 +74,15 @@ addEventListener('backgroundfetched', event => {
 
     await cache.put(request, response);
     event.updateUI('Downloaded video');
+
+    const channel = new BroadcastChannel('channel');
+    channel.postMessage({ status: 'Downloaded video' });
   }());
 });
+
+addEventListener('message', event => {
+  const channel = new BroadcastChannel('channel');
+  useArrayBuffer = (event.data.action == 'arraybuffer');
+  channel.postMessage({ status: 'Changed useArrayBuffer to ' + useArrayBuffer });
+});
+
